4acfe3436f4f5d0491b0814bd1a09b9bff3eedc1
cloudkj <cloudkj@users.noreply.github.com>
2021-03-01 00:23:23 -0800

    Load ROM from command line arg

--- src/main.scm
+++ src/main.scm
@@ -2,39 +2,36 @@
 
 (use srfi-4)
 
-;; Initialization
-(init-display)
+(define (load-rom filename)
+  (let ((port (open-input-file filename)))
+    (let loop ((i 0))
+      (let ((c (read-char port)))
+        (unless (eof-object? c)
+          (u8vector-set! *ram* (+ *PC* i) (char->integer c))
+          (loop (+ i 1)))))))
 
-;; Load test ROM
-;; TODO: load rom from command line arg
-(let ((port (open-input-file "test_opcode.ch8")))
-;;(let ((port (open-input-file "c8_test.c8")))
-  (let loop ((i 0))
-    (let ((c (read-char port)))
-      (unless (eof-object? c)
-        (u8vector-set! *ram* (+ *PC* i) (char->integer c))
-        (loop (+ i 1))))))
-
-;; Execution
-;; TODO: implement clock rate
 (define (execute)
+  ;; TODO: implement clock rate
   (let* ((msb (u8vector-ref *ram* *PC*))
          (lsb (u8vector-ref *ram* (+ *PC* 1)))
          (op (or (jump-ops msb lsb) (ops msb lsb))))
     (if op
         (begin
           (refresh-display)
-          (print-instruction msb lsb #f)
-;;          (print-registers) (print)
           (if (> *DT* 0) (set! *DT* (- *DT* 1)))
           (if (> *ST* 0) (set! *ST* (- *ST* 1)))
-          (op))
+          (op)
+          (execute))
         (begin
           (print-instruction msb lsb "Instruction not found")
-          (print-registers)
-          #f))))
+          (print-registers)))))
 
-;; Main loop
-(let loop ()
-  (if (execute)
-      (loop)))
+;;; Run
+
+(let* ((args (command-line-arguments))
+       (filename (if (= (length args) 0) #f (car args))))
+  (if filename
+      (begin
+        (load-rom filename)
+        (init-display)
+        (execute))))
